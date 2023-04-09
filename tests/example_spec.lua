56f3815e190f09086764175607c166dd95fa1cfc
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-04-09 17:41:08 +0200

    test: parameter processing test

--- tests/example_spec.lua
+++ tests/example_spec.lua
@@ -1,14 +1,36 @@
-describe("some basics", function()
-	local bello = function(boo) return "bello " .. boo end
+describe("parameter processing: ", function()
+	local parameters = require("alt-substitute.process-parameters")
 
-	local bounter
-
-	before_each(function() bounter = 0 end)
-
-	it("some test", function()
-		bounter = 100
-		assert.equals("bello Brian", bello("Brian"))
+	it("Standard Case", function()
+		local input = "/search/repl/g"
+		local params = parameters.splitByUnescapedSlash(input)
+		assert.equals("search", params[1])
+		assert.equals("repl", params[2])
+		assert.equals("g", params[3])
 	end)
 
-	it("some other test", function() assert.equals(0, bounter) end)
+	it("escaping the slash", function()
+		local input = [[/some\/path/repl/g]]
+		local params = parameters.splitByUnescapedSlash(input)
+		assert.equals("some/path", params[1])
+		assert.equals("repl", params[2])
+		assert.equals("g", params[3])
+	end)
+
+	it("empty replacement", function()
+		local input = [[/foobar//i]]
+		local params = parameters.splitByUnescapedSlash(input)
+		assert.equals("foobar", params[1])
+		assert.equals("", params[2])
+		assert.equals("i", params[3])
+	end)
+
+	it("no flags", function()
+		local input = [[/foo/bar/]]
+		local params = parameters.splitByUnescapedSlash(input)
+		assert.equals("foo", params[1])
+		assert.equals("bar", params[2])
+		assert.equals(nil, params[3])
+	end)
+
 end)
