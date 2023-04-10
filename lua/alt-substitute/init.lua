1bb1207148bdfca3bfc4d056ee4cfa9c81d5c8d4
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-04-10 14:20:52 +0200

    refactor: confirm and preview in separate module

--- lua/alt-substitute/init.lua
+++ lua/alt-substitute/init.lua
@@ -1,123 +1,32 @@
 local M = {}
-local warn = vim.log.levels.WARN
-local hlgroup = "Substitute"
-local regexFlavor, showNotification
-
-local parameters = require("alt-substitute.process-parameters")
-local regex = require("alt-substitute.regex")
-
---------------------------------------------------------------------------------
-
----the substitution to perform when the commandline is confirmed with <CR>
----@param opts table
-local function confirmSubstitution(opts)
-	local curBufNum = vim.api.nvim_get_current_buf()
-	local line1, line2, bufferLines, toSearch, toReplace, flags = parameters.process(opts, curBufNum)
-	local validFlags = "gfi"
-	local invalidFlagsUsed = flags:find("[^" .. validFlags .. "]")
-
-	if not toReplace then
-		vim.notify("No replacement value given, cannot perform substitution.", warn)
-		return
-	elseif toReplace:find("%%$") then
-		-- stylua: ignore
-		vim.notify('A single "%" cannot be used as replacement value in lua patterns. \n(A literal "%" must be escaped as "%%".)', warn)
-		return
-	elseif toSearch == "" then
-		vim.notify("Search string is empty.", warn)
-		return
-	end
-	if invalidFlagsUsed then
-		-- stylua: ignore
-		vim.notify(('"%s" contains invalid flags, the only valid flags are "%s".\nInvalid flags have been ignored.'):format(flags, validFlags), warn)
-	end
-
-	local newBufferLines, totalReplacementCount =
-		regex.replace(bufferLines, toSearch, toReplace, nil, flags, regexFlavor)
-	vim.api.nvim_buf_set_lines(curBufNum, line1 - 1, line2, false, newBufferLines)
-	if showNotification then
-		vim.notify("Replaced " .. tostring(totalReplacementCount) .. " instances.")
-	end
-end
-
--- https://neovim.io/doc/user/map.html#%3Acommand-preview
----@param opts table
----@param ns number namespace for the highlight
----@param _ any unused, is bufnr if inccommand=split
----@return 0|1|2 -- no preview|nosplit|split/nosplit
-local function previewSubstitution(opts, ns, _)
-	local curBufNum = vim.api.nvim_get_current_buf()
-	local line1, line2, bufferLines, toSearch, toReplace, flags = parameters.process(opts, curBufNum)
-
-	if toSearch == "" then return 0 end -- without search value, there will be no useful preview
-
-	-- PREVIEW CHANGES
-	if toReplace and toReplace ~= "" then
-		local newBufferLines = regex.replace(bufferLines, toSearch, toReplace, nil, flags, regexFlavor)
-		vim.api.nvim_buf_set_lines(curBufNum, line1 - 1, line2, false, newBufferLines)
-	end
-
-	-- ADD HIGHLIGHTS
-	-- iterate lines
-	for i, line in ipairs(bufferLines) do
-		local lineIdx = line1 + i - 2
-
-		-- search line for matches
-		local matchesInLine = {}
-		local startPos, endPos = 0, 0
-		while true do
-			startPos, endPos = regex.find(line, toSearch, endPos + 1, flags, regexFlavor)
-			if not startPos then break end -- no more matches found
-			table.insert(matchesInLine, { startPos = startPos, endPos = endPos })
-			if not (flags:find("g")) then break end -- only one iteration when no `g` flag
-		end
-
-		-- iterate matches
-		local previousShift = 0
-		for ii, m in ipairs(matchesInLine) do
-			-- if replacing, needs to consider shift in positions from previous
-			-- matches in the same line
-			if toReplace and toReplace ~= "" then
-				-- stylua: ignore
-				local lineWithSomeSubs = (regex.replace({ line }, toSearch, toReplace, ii, flags, regexFlavor))[1]
-				local diff = (#lineWithSomeSubs - #line)
-				m.startPos = m.startPos + previousShift
-				m.endPos = m.endPos + diff
-				previousShift = diff
-			end
-
-			vim.api.nvim_buf_add_highlight(curBufNum, ns, hlgroup, lineIdx, m.startPos - 1, m.endPos)
-		end
-	end
-
-	return 1 -- 1 = always act as if inccommand=unsplit
-end
-
 --------------------------------------------------------------------------------
 
 ---@class config table containing the options for the plugin
----@field regexFlavor string default: lua
 ---@field showNotification boolean whether to show the "x replacements made" notice, default: true
 
----@param opts? config
-function M.setup(opts)
+---@param config? config
+function M.setup(config)
 	-- default values
-	if not opts then opts = {} end
-	regexFlavor = "lua" -- for now only lua is supported
-	showNotification = opts.showNotification or true
+	if not config then config = {} end
+	local regexFlavor = "lua" -- in case other regex flavors get added later
+	local showNotification = config.showNotification or true
+	local substitution = require("alt-substitute.substitution")
 
 	-- setup user commands
 	local commands = { "S", "AltSubstitute" }
-	for _, cmd in pairs(commands) do
-		vim.api.nvim_create_user_command(cmd, confirmSubstitution, {
-			nargs = "?",
-			range = "%", -- defaults to whole buffer
-			addr = "lines",
-			preview = previewSubstitution,
-		})
+	for _, exCmd in pairs(commands) do
+		vim.api.nvim_create_user_command(
+			exCmd,
+			function(opts) substitution.confirm(opts, showNotification, regexFlavor) end,
+			{
+				nargs = "?",
+				range = "%", -- defaults to whole buffer
+				addr = "lines",
+				preview = function(opts, ns, _) return substitution.preview(opts, ns, regexFlavor) end,
+			}
+		)
 	end
 end
 
 --------------------------------------------------------------------------------
-
 return M
---
+++ lua/alt-substitute/substitution.lua
@@ -0,0 +1,98 @@
+local M = {}
+--------------------------------------------------------------------------------
+local warn = vim.log.levels.WARN
+local hlgroup = "Substitute"
+
+local parameters = require("alt-substitute.process-parameters")
+local regex = require("alt-substitute.regex")
+
+--------------------------------------------------------------------------------
+
+-- https://neovim.io/doc/user/map.html#%3Acommand-preview
+---@param opts table
+---@param ns number namespace for the highlight
+---@return 0|1|2 -- no preview|nosplit|split/nosplit
+function M.preview(opts, ns, regexFlavor)
+	local curBufNum = vim.api.nvim_get_current_buf()
+	local line1, line2, bufferLines, toSearch, toReplace, flags = parameters.process(opts, curBufNum)
+
+	if toSearch == "" then return 0 end -- without search value, there will be no useful preview
+
+	-- PREVIEW CHANGES
+	if toReplace and toReplace ~= "" then
+		local newBufferLines = regex.replace(bufferLines, toSearch, toReplace, nil, flags, regexFlavor)
+		vim.api.nvim_buf_set_lines(curBufNum, line1 - 1, line2, false, newBufferLines)
+	end
+
+	-- ADD HIGHLIGHTS
+	-- iterate lines
+	for i, line in ipairs(bufferLines) do
+		local lineIdx = line1 + i - 2
+
+		-- search line for matches
+		local matchesInLine = {}
+		local startPos, endPos = 0, 0
+		while true do
+			startPos, endPos = regex.find(line, toSearch, endPos + 1, flags, regexFlavor)
+			if not startPos then break end -- no more matches found
+			table.insert(matchesInLine, { startPos = startPos, endPos = endPos })
+			if not (flags:find("g")) then break end -- only one iteration when no `g` flag
+		end
+
+		-- iterate matches
+		local previousShift = 0
+		for ii, m in ipairs(matchesInLine) do
+			-- if replacing, needs to consider shift in positions from previous
+			-- matches in the same line
+			if toReplace and toReplace ~= "" then
+				-- stylua: ignore
+				local lineWithSomeSubs = (regex.replace({ line }, toSearch, toReplace, ii, flags, regexFlavor))[1]
+				local diff = (#lineWithSomeSubs - #line)
+				m.startPos = m.startPos + previousShift
+				m.endPos = m.endPos + diff
+				previousShift = diff
+			end
+
+			vim.api.nvim_buf_add_highlight(curBufNum, ns, hlgroup, lineIdx, m.startPos - 1, m.endPos)
+		end
+	end
+
+	return 1 -- 1 = always act as if inccommand=unsplit
+end
+
+---the substitution to perform when the commandline is confirmed with <CR>
+---@param opts table
+---@param showNotification boolean
+---@param regexFlavor string currently only lua
+function M.confirm(opts, showNotification, regexFlavor)
+	local curBufNum = vim.api.nvim_get_current_buf()
+	local line1, line2, bufferLines, toSearch, toReplace, flags = parameters.process(opts, curBufNum)
+	local validFlags = "gfi"
+	local invalidFlagsUsed = flags:find("[^" .. validFlags .. "]")
+
+	if not toReplace then
+		vim.notify("No replacement value given, cannot perform substitution.", warn)
+		return
+	elseif toReplace:find("%%$") then
+		-- stylua: ignore
+		vim.notify('A single "%" cannot be used as replacement value in lua patterns. \n(A literal "%" must be escaped as "%%".)', warn)
+		return
+	elseif toSearch == "" then
+		vim.notify("Search string is empty.", warn)
+		return
+	end
+	if invalidFlagsUsed then
+		-- stylua: ignore
+		vim.notify(('"%s" contains invalid flags, the only valid flags are "%s".\nInvalid flags have been ignored.'):format(flags, validFlags), warn)
+	end
+
+	local newBufferLines, totalReplacementCount =
+		regex.replace(bufferLines, toSearch, toReplace, nil, flags, regexFlavor)
+	vim.api.nvim_buf_set_lines(curBufNum, line1 - 1, line2, false, newBufferLines)
+	if showNotification then
+		vim.notify("Replaced " .. tostring(totalReplacementCount) .. " instances.")
+	end
+end
+
+--------------------------------------------------------------------------------
+return M
